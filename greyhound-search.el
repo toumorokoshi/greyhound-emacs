54a6ab199574a362350f98a05ed781ebd31cd93e
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-10-02 22:12:01 -0700

    fleshing out the interface a bit more

--- greyhound-search.el
+++ greyhound-search.el
@@ -2,6 +2,7 @@
 ;; bugs to fix in libraries
 ;; * bug with google's WebSocket code: expects camelcase websocket name in 'Upgrade', not required.
 ;; * 'origin' on emacs websocket needs an http attached to it...
+
 (require 'websocket)
 (require 'cl)
 
@@ -9,17 +10,19 @@
 (defvar greyhound/messages nil)
 (defvar greyhound/closed nil)
 (defvar greyhound/process nil)
+; (defvar greyhound/executable "greyhound-search")
+(defvar greyhound/executable "/home/tsutsumi/workspace/greyhound-search/greyhound-search")
 
 (setq websocket-debug t)
 
-(defvar greyhound/start
-  (websocket-open 
-   greyhound/server
-   :on-message (lambda (websocket frame)
-                 (push (websocket-frame-payload frame) greyhound/messages)
-                 (message "ws frame: %S" (websocket-frame-payload frame))
-                 (error "Test error (expected)"))
-   :on-close (lambda (websocket) (setq greyhound/closed t))))
+;; (defvar greyhound/start
+;;   (websocket-open 
+;;    greyhound/server
+;;    :on-message (lambda (websocket frame)
+;;                  (push (websocket-frame-payload frame) greyhound/messages)
+;;                  (message "ws frame: %S" (websocket-frame-payload frame))
+;;                  (error "Test error (expected)"))
+;;    :on-close (lambda (websocket) (setq greyhound/closed t))))
 
 (defun greyhound/start ()
   "Start and start the websocket communication with the greyhound server"
@@ -35,18 +38,24 @@
   (greyhound/stop-server)
 )
 
-(websocket-openp greyhound/start)
-(websocket-send-text greyhound/start "{\"action\": \"query\", \"queryData\": {\"project\": \"statics\", \"query\": \"t\"}}")
+;;(websocket-openp greyhound/start)
+;;(websocket-send-text greyhound/start "{\"action\": \"query\", \"queryData\": {\"project\": \"statics\", \"query\": \"t\"}}")
 
 ;; greyhound server methods
 
 (defun greyhound/start-server ()
   "start the greyhound server"
-)
+  (unless greyhound/process 
+    (setq (start-process 
+           "greyhound" 
+           "*greyhound*" 
+           greyhound/executable))))
 
 (defun greyhound/stop-server ()
   "stop the greyhound server"
-)
+  (if greyhound/process
+    (setq greyhound/process
+          (stop-process greyhound/process))))
 
 ;; greyhound websocket methods
 
